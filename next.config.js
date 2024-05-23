4f84c33e01585e9cf6262692df49edc5d22b220e
Mohammed Monirul Islam <lalumastan@gmail.com>
2024-05-22 20:08:02 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'icsdiscover.great-site.net',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
-  },
   reactStrictMode: true,
 }
 
