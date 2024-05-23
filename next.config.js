afed9fb7e3849c2202833f4e83cc9782bbcbd655
Mohammed Monirul Islam <lalumastan@gmail.com>
2024-05-22 20:05:07 -0400

    updated the common image references

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'icsdiscover.great-site.net',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
   reactStrictMode: true,
 }
 
